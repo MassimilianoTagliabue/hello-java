248dbf26e166ca7dd6ff24b903d61c9b265e437b
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2025-03-12 12:25:37 +0100

    feat: test comandi git

--- src/org/lessons/java/StampaSaluto.java
+++ src/org/lessons/java/StampaSaluto.java
@@ -4,5 +4,7 @@ public class StampaSaluto {
 
     public static void main(String[] args) {
         System.out.println("ciao, sono il saluto di prova");
+
+        System.out.println("prova comandi");
     }
 }
